06cef5e83204792d1a929645d6fff949392d846c
Tushar Roy <xxcrack@gmail.com>
2014-03-25 22:08:31 +0600

    changed

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,11 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 var id = 'que';
 var count = 1;
-
 function onNext() {
     if (count >= 1 && count < 20) {
         previous = '#' + id + count;
@@ -15,12 +9,25 @@ function onNext() {
         $(previous).hide();
         $('#next').attr("disabled", true);
     }
-    else{
+    else {
         $('#next').hide();
         $('#submit').show();
     }
 }
-function onChange(){
+function onChange() {
     $('#next').removeAttr("disabled");
 }
-
+function countResult() {
+    var correctAns=0;
+    var answers = ["15","8","6","5","4","1","2","opt1","b","b","c","a","a","a","c","d","c","a","d","d"];
+    for (var i = 0; i < 20; i++) {
+        var i2=i+1;
+        var selected = id + i2;
+        var text = $('input[name="' + selected + '"]:checked').val();
+        if(answers[i]===text){
+            correctAns++;
+        }
+    }
+    //alert(correctAns);
+    return correctAns;
+}
