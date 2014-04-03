52681e2fe971e81a75cc3ac2f9e8d0b6fe605cf1
Tushar Roy <xxcrack@gmail.com>
2014-04-03 14:15:26 +0600

    changed

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,6 +1,12 @@
 var id = 'que';
 var count = 1;
+
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+}
+
 function onNext() {
+
     if (count >= 1 && count < 20) {
         previous = '#' + id + count;
         count = count + 1;
@@ -11,23 +17,92 @@ function onNext() {
     }
     else {
         $('#next').hide();
-        $('#submit').show();
     }
 }
 function onChange() {
     $('#next').removeAttr("disabled");
 }
 function countResult() {
-    var correctAns=0;
-    var answers = ["15","8","6","5","4","1","2","opt1","b","b","c","a","a","a","c","d","c","a","d","d"];
+    var correctAns = 0;
+    var answers = ["15", "8", "6", "5", "4", "1", "2", "opt1", "b", "b", "c", "a", "a", "a", "c", "d", "c", "a", "d", "d"];
     for (var i = 0; i < 20; i++) {
-        var i2=i+1;
+        var i2 = i + 1;
         var selected = id + i2;
         var text = $('input[name="' + selected + '"]:checked').val();
-        if(answers[i]===text){
+        if (answers[i] === text) {
             correctAns++;
         }
     }
     //alert(correctAns);
     return correctAns;
+}
+
+function displayFrench(){
+    $('.German').hide();
+    $('.French').show();
+}
+function displayGerman(){
+    $('.French').hide();
+    $('.German').show();
+}
+function langSelector(){
+    var userLang = navigator.language || navigator.userLanguage;
+    if(userLang=='fr'||userLang=='fr-BE'||userLang=='fr-CA'||userLang=='fr-FR'||userLang=='fr-LU'||userLang=='fr-MC'||userLang=='fr-CH'){
+        displayFrench();
+    }
+    else{
+        displayGerman();
+    }
+}
+
+function playAgain() {
+    var dynamicData = 'p=' + getURLParameter("p");
+    $.ajax({
+        dataType: "text",
+        crossDomain: true,
+        type: "get",
+        url: "/quiz/pa",
+        data: dynamicData,
+        success: function(xml) {
+
+            if (status == 200) {
+
+            }
+
+        },
+        error: function(request, status, error) {
+
+        }
+    });
+    
+    $("#scoreMessage").hide();
+    
+    langSelector();
+
+}
+
+function saveResult() {
+
+    var dynamicData = 'p=' + getURLParameter("p") + '&s=' + numberOfQuestions + '&c=' + countResult();
+
+    var score = countResult() + '/' + numberOfQuestions;
+
+    $.ajax({
+        dataType: "text",
+        crossDomain: true,
+        type: "get",
+        url: "/quiz/res",
+        data: dynamicData,
+        success: function(xml) {
+
+            $("#scoreNum").html(score);
+            $("#scoreMessage").show();
+            $(".controls").hide();
+            $("#questForm").hide();
+
+        },
+        error: function(request, status, error) {
+
+        }
+    });
 }
